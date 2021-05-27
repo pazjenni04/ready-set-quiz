60071be26e6c4bcb2c05e3aa4bf642b2943ff8d1
Jenni Davila <pazjenni1331@gmail.com>
2021-05-26 23:43:55 -0500

    finally able to get option buttons from the array to display in function getRandomQ()

--- myscript.js
+++ myscript.js
@@ -3,6 +3,9 @@ var question2 = document.getElementsByClassName(".question-b");
 var question3 = document.getElementsByClassName(".question-c");
 var question4 = document.getElementsByClassName(".question-d");
 
+
+var score = 0; //sets original score to 0
+
 //objects created for question bank
 var quest1Data = {
   question: "Which of the following is not an arithmetic operator?",
@@ -13,7 +16,7 @@ var quest1Data = {
 var quest2Data = {
   question: "What is a boolean?",
   options: ["a standalone entity that holds multiple values in terms of properties and methods", "an operator that perform addition, subtraction, multiplication, division, exponentiation, and modulus operations", "a binary variable, having two possible values called 'true' and 'false'", "none of the above"],
-  answer: "a binary variable, having two possible values called 'true' and 'false'"
+  answer: "a binary variable, having two possible values called 'true' and 'false'",
 };
 
 var quest3Data = {
@@ -42,7 +45,7 @@ function countdown() {
     
     var timeInterval = setInterval(function() {
       timeLeft --;
-      timeEl.textContent = timeLeft + " seconds remaining"
+      timeEl.textContent = timeLeft + " seconds remaining";
   
       if (timeLeft === 0) {
         clearInterval(timeInterval);
@@ -52,19 +55,12 @@ function countdown() {
     }, 1000);
   };
 
-function displayScores() {
-    var input = getElementById("scoreboard");
-    input.setAttribute('type', 'text');
-    input.setAttribute('value','default');
-
-};
 
 function startQuiz() {
   countdown();
   document.querySelector(".homepage").style.display = "none"; //this removes the instructional page of "hides"
   getRandomQ();
 };
-
 
 //start the timer
 //switch from the opening window (instructional page) and display first question and options
@@ -73,38 +69,35 @@ function startQuiz() {
 //show list of highscores -- local storage
 
 
-//this function will make content hidden or visible
-//function displayContent(event)
-
-
-
-
-
-
 //this function will assist in generating a random order in which the questions will get presented
-function getRandomQ (event) {
-    var currentQuestion = arrayofQ[0];  //this will be the question that is located inside the index depending on its position
-    question1.textContent =  currentQuestion.question //allows for question to display on window
-  
-  //loop over the current questions options
-  //create a button for each option
-  //add the option text into that button
-  //add the correct answer data-attribute to the button
-  //add that button onto the page
+function getRandomQ () {
+    var currentQuestion = arrayofQ[3];  //this will be the question that is located inside the index depending on its position
+    question1.textContent =  currentQuestion.question; //allows for question to display on window
+
    for(var i=0; i<currentQuestion.options.length; i++) { //this creates the for loop for the options within the question
-      var buttonOptions = document.createElement("button");
-      buttonOptions.textContent = currentQuestion.options[1];
-      buttonOptions.dataset.correctAnswer = currentQuestion.answer;
+      var buttonOptions = document.createElement("button");  //created a button for each option within the array
+      buttonOptions.textContent = currentQuestion.options[i];  //shows all answers dedicated to the array within "options"
+      document.body.appendChild(buttonOptions);  //displays button options onto page
 
-      //displaying button onto page
-     
-       
-    
-    
+      buttonOptions.dataset.correctAnswer = currentQuestion.answer;  //adds the correct answer data-attribute to the button
+      
+      //buttonOptions.addEventListener("click",  
+      //need to create an event listener that when push the button it will register whether answer was correct and go to the next quest in the queue
+
     //generatedQ += questions.charAt[Math.floor(Math.random(questions.length))]
-
-    };
+     
 };
-//first pull a question from the array
-//pull in a random order
-//display the question onto the screen
+
+};
+
+function displayScores() {
+  var userScore = document.querySelector(".score-container");
+  var displaySettings = userScore.style.display;
+  
+  if (countdown() > 0){
+    userScore
+  }
+
+displayScores();
+
+};

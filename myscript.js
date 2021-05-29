6e3d96e60a7aa4094996ebb8b9499bc30e201f52
Jenni Davila <pazjenni1331@gmail.com>
2021-05-28 22:17:28 -0500

    Updated for scoreboard to be hidden until timer runs out then will be the only thing displayed onto the page.  Added some CSS styles to the page.

--- myscript.js
+++ myscript.js
@@ -1,7 +1,5 @@
 var question1 = document.querySelector(".question-a");
-var question2 = document.getElementsByClassName(".question-b");
-var question3 = document.getElementsByClassName(".question-c");
-var question4 = document.getElementsByClassName(".question-d");
+var quizEl = document.querySelector(".quizEl");
 
 var questionIndex = 0; //this starts at first question
 
@@ -29,7 +27,7 @@ var quest3Data = {
 
 var quest4Data = {
   question: "What is the purpose of a for-loop?",
-  options: ["loops through a block of code a number of times", "it demonstrates the amount of indexes in an array", "none of the above", "it allows you to to connect a javascript code to a particular class or id on the HTML document."],
+  options: ["loops through a block of code a number of times", "it demonstrates the amount of indexes in an array", "none of the above", "it allows you to to connect a javascript code to a particular class or id in the HTML document."],
   answer: "loops through a block of code a number of times",
 }
 
@@ -39,7 +37,7 @@ var arrayofQ = [quest1Data, quest2Data, quest3Data, quest4Data];
 //event listener starts once the user clicks the start button
 document.getElementById("mybutton").addEventListener("click", startQuiz);
 
-var timeLeft = 10;
+var timeLeft = 5;
 var timeEl = document.querySelector(".timer");
 
 //this function runs the timer for the quiz -- once the user clicks the start button the countdown will begin
@@ -47,7 +45,7 @@ function countdown() {
     
     var timeInterval = setInterval(function() {
       timeLeft --;
-      timeEl.textContent = timeLeft + " seconds remaining";
+      timeEl.textContent = "Timer: " + timeLeft + " seconds remaining";
   
       displayScores();
 
@@ -76,23 +74,23 @@ function startQuiz() {
 //this function will assist in generating a random order in which the questions will get presented
 function getRandomQ () {
     var currentQuestion = arrayofQ[questionIndex];  //this will be the question that is located inside the index depending on its position
-    question1.textContent =  currentQuestion.question; //allows for question to display on window
 
-    
-    
+    question1.textContent = currentQuestion.question; //allows for question to display on window
 
    for(var i=0; i<currentQuestion.options.length; i++) { //this creates the for loop for the options within the question
       var buttonOptions = document.createElement("button");  //created a button for each option within the array
+      var hiddenEl = document.querySelector(".hidden");
+      
       buttonOptions.textContent = currentQuestion.options[i];  //shows all answers dedicated to the array within "options"
-      document.body.appendChild(buttonOptions);  //displays button options onto page
+      hiddenEl.appendChild(buttonOptions);  //displays button options onto page
       
       buttonOptions.onclick = optionClick; //creates event listener for when you click the button it connects to the function () optionClick
 
-      currentQuestion.innerHTML = ""; //should clear buttons once goes to the next question
-
       buttonOptions.dataset.correctAnswer = currentQuestion.answer;  //adds the correct answer data-attribute to the button
       
+      currentQuestion.options.innerHTML = " "; //needs to be able to clear the interval - but doesn't work?
 };
+
 };
 
 
@@ -100,23 +98,21 @@ function displayScores() {
   var userScore = document.querySelector(".score-container");
   
   if (timeLeft > 0){
-    userScore.style.display = "none";
+    userScore.style.display = "none"; //if timer is greater than 0 then display score will be hidden
   } else {
-    userScore.style.display = "block";
+    userScore.style.display = "block"; //displays the scoreboard onto the page once timer hits 0
+    document.querySelector(".hidden").style.display = "none"; //hides all elements inside div container class ."hidden" -- all questions and headers on page
   };
-
-
 };
 
 displayScores();
 
-function endQuiz () {
-  clearInterval(timeLeft)
-};
-
 function optionClick (event) {
+  event.preventDefault();
+
   var currentQuestion = arrayofQ[questionIndex];
   var choiceClick = event.target.textContent;
+  
 
   if (choiceClick === currentQuestion.answer) {
     console.log("Correct!");
@@ -124,8 +120,35 @@ function optionClick (event) {
     console.log("Wrong!");
     countdown -2;
   };
+  
   questionIndex++;
   getRandomQ();
-
 };
 
+
+
+
+//create a scoreboard to collect 2 points for every question correct
+//need to calculate all scores at the end
+//need to add score to local storage and name input once complete
+
+var scoreEl = 0;
+
+function addToScore () {
+  var scoreboard = document.getElementById("#scoreboard");
+  var currentQuestion = arrayofQ[0];
+  var answer = currentQuestion.answer;
+
+  scoreboard.textContent = document.querySelector(".container")
+
+  if (optionClick === currentQuestion.answer) {
+    scoreEl +2;
+
+  } else {
+
+    return;
+  };
+  
+};
+
+addToScore();
